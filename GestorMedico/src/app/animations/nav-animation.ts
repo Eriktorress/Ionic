01f870c1d335f933bba8a1a3f6af39e4c481eb7a
Nacho <ig.dinamarca@duocuc.cl>
2022-08-30 17:37:41 -0400

    animacion flecha volver + comentarios

--- GestorMedico/src/app/animations/nav-animation.ts
+++ GestorMedico/src/app/animations/nav-animation.ts
@@ -2,7 +2,7 @@ import { AnimationController,Animation } from "@ionic/angular";
 
 export const enterAnimation =(baseEl:HTMLElement, opts?: any): Animation=>{    //estamos creando una animacion propia la cual exportamos ael app.modules(modulo de aplicacion) 
 
-    const DURATION =400; //definimos la duracion de la transicion que haremos con animacion en la pagina
+    const DURATION =400; //definimos la variable de duracion de la transicion que haremos con animacion en la pagina
 
     //se crean estas restriciones para que el sistema no se caiga por completo 
     console.log('baseEl:',baseEl);
@@ -10,26 +10,26 @@ export const enterAnimation =(baseEl:HTMLElement, opts?: any): Animation=>{    /
 
     const animationCtrl = new AnimationController();
 
-    if (opts.direction ==='forward'){
+    if (opts.direction ==='forward'){                     // forward= delantera
         return animationCtrl.create()
         .addElement(opts.enteringEl)
         .duration(DURATION)
         .easing('ease-in')
         .fromTo('opacity',0,1);
-    }else{
-        const rootAnimation = animationCtrl.create() //animacion de ingreso 
-        .addElement(opts.enteringEl) 
+    }else {
+        const rootAnimation = animationCtrl.create()      //animacion de ingreso 
+        .addElement(opts.enteringEl)                      //elemento de entrada
         .duration(DURATION)                              //duracion de la transicion
-        .easing('ease-in')
-        .fromTo('opacity',0,1);
+        .easing('ease-in')                               //efecto transicion de entrada
+        .fromTo('opacity',0,1);                          //opacidad
 
         const leavingAnimation = animationCtrl.create() //animacion de volver atras 
-        .addElement(opts.LeavingEl)
+        .addElement(opts.leavingEl)                     //elemento de salida 
         .duration(DURATION)                             //duracion de la transicion
-        .easing('ease-out')
-        .fromTo('opacity',0,1);
+        .easing('ease-out')                             //efecto de trancision salida
+        .fromTo('opacity',1,0);                         //opacidad
 
-        return animationCtrl.create().addAnimation([rootAnimation,leavingAnimation]);
+        return animationCtrl.create().addAnimation([rootAnimation, leavingAnimation]);
     }
     
 }
