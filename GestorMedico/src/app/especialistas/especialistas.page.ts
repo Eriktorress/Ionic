3b9f1fa38c83a04af9cccf951f4ba57faf74f24b
Erik <eri.torres@duocuc.cl>
2022-08-31 17:45:52 -0400

    Css

--- GestorMedico/src/app/especialistas/especialistas.page.ts
+++ GestorMedico/src/app/especialistas/especialistas.page.ts
@@ -23,8 +23,56 @@ export class EspecialistasPage implements OnInit {
       especialidad: 'Doctor cirujano',
       informacion: ["comentarios","comentario 2"]
 
-    }
+    },
+    {
+      id:'3',
+      nombre:'Gregory House',
+      url:"https://joacosoft.wordpress.com/files/2009/07/dr-house.jpg",
+      especialidad: 'Diagnostico medico',
+      informacion: ["comentarios","comentario 2"]
 
+    },
+    {
+      id:'4',
+      nombre:'Max Goodwin',
+      url:"https://es.web.img2.acsta.net/c_162_216/pictures/19/06/22/13/38/1944794.jpg",
+      especialidad: 'Director medico',
+      informacion: ["comentarios","comentario 2"]
+
+    },
+    {
+      id:'5',
+      nombre:'Ignatius Froome',
+      url:"https://bellomag.com/wp-content/uploads/2020/01/Screen-Shot-2020-01-14-at-3.44.52-PM.png",
+      especialidad: 'Psiquiatra',
+      informacion: ["comentarios","comentario 2"]
+
+    },
+    {
+      id:'6',
+      nombre:'Lauren Bloom',
+      url:"https://elcomercio.pe/resizer/9mb2qJ2hyeftMPwcj2GoXIxl_pM=/620x0/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/TVLGP36HVRE37JE2U3IJOQNCT4.jpg",
+      especialidad: 'Urgencia',
+      informacion: ["comentarios","comentario 2"]
+
+    },
+    {
+      id:'7',
+      nombre:'Floyd Reynolds',
+      url:"https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/new-amsterdam-floyd-reynolds-jocko-sims-1614792240.jpg?crop=0.670xw:1.00xh;0.0340xw,0&resize=480:*",
+      especialidad: 'Cirujano cardiologo',
+      informacion: ["comentarios","comentario 2"]
+
+    },
+    {
+      id:'8',
+      nombre:'Shaun Murphy',
+      url:"https://redgol.cl/__export/1659645791786/sites/redgol/img/2022/08/04/thegooddoctr_crop1645628864083_crop1659645791100.png_1159711837.png",
+      especialidad: 'Diagnostico medico',
+      informacion: ["comentarios","comentario 2"]
+
+    }
+    
 
   
   ];
